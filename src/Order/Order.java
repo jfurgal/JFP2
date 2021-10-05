542f17642828d2dec05afbd84fc7925962d57a03
jfurgal <90795741+jfurgal@users.noreply.github.com>
2021-10-05 14:47:32 -0400

    progress...

--- src/Order/Order.java
+++ src/Order/Order.java
@@ -3,11 +3,24 @@ package Order;
 import Customer.Customer;
 import ShippingAddress.ShippingAddress;
 
+import java.lang.reflect.Constructor;
+
 public class Order {
+    private final Constructor orderBy;
     private ShippingAddress destination;
     private Customer orderedBy;
 
-    public Order(ShippingAddressdestination, Customer orderedBy)
-
+    public Order(ShippingAddress destination, Customer orderedBy){
+        super();
+        this.destination = destination;
+        this.orderBy = orderBy;
+    }
+    
+    public String getDestination(){
+        return destination.toString();
+    }
+    
+    public String getOrderBy(){
+        return orderBy.toString();
     }
 }
